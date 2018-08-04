4670693321426822500b41de26ae33112d10387b
crazyzhou <fenlary.zhou@gmail.com>
2018-08-04 16:21:09 +0800

    更新入口文件

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 "use strict";
 
 const http = require("http");
+const path = require("path");
 const koa = require("koa");
-const koaStatic = require("koa-static");
-const koaRouter = require("koa-router");
+const statics = require("koa-static");
+const views = require("koa-views");
 const config = require("./config.default");
-const Log = require("./common/log");
+const Log = require("./logs/log");
 const router = require("./router");
 
 
 let app = new koa();
 
+app.use(views(path.join(__dirname,"view"),{map:{html:"jade"}}));
+app.use(statics(path.join(__dirname,"static")));
+
+app.use(router.routes()).use(router.allowedMethods());
 
 
 
 
 
 
-const server = http.createServer(app.callback(),()=>{
-    const host = server.address.host;
-    const port = server.address.post;
-    
 
-}).listen(config.port,config.host);
+const server = http.createServer(app.callback()).listen(config.port,config.host);
 
-console.log(`The server is running on ${config.host}:${config.port}`);
+Log.debug(`The server is running on ${config.host}:${config.port}`);
 
