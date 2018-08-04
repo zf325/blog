5388d1c289d017b5ddf175c38f93028e129d08b2
crazyzhou <fenlary.zhou@gmail.com>
2018-08-04 16:22:54 +0800

    项目路由

--- router.js
+++ router.js
@@ -1,2 +1,17 @@
 "use strict";
 
+const Router = require("koa-router");
+
+
+let router = new Router();
+router.get("/test",async(ctx,next)=>{
+    ctx.body = {code:0};
+});
+router.get("/",async(ctx,next)=>{
+    await ctx.render("index.jade");
+});
+
+router.get('/categoty/article');
+
+
+module.exports = router;
