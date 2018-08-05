1e5a56d487d250be5da9410be265be0c3484fb0c
crazyzhou <fenlary.zhou@gmail.com>
2018-08-05 11:08:48 +0800

    日志记录打印，按月生成日志文件当前目录

--- logs/log.js
+++ logs/log.js
@@ -4,6 +4,8 @@
  * 
  */
 //todo 设计 log 结构，方法，形式 
+const fs = require("fs");
+const path = require("path");
 
 function Log(){
     this.message = errMsg;
@@ -16,15 +18,22 @@ Log.debug = (desc,errMsg)=>{
 Log.info = (desc)=>{
     const env = process.env.NODE_ENV||"development";
     const temp = `[${new Date().toLocaleString()}][info] ${desc}`;
-    if(env === 'development'){
+    if(env === 'development1'){
         console.log(temp);
     }else{
         Log.writeLog(temp);
     }
 }
 
-Log.writeLog = (desc,err)=>{
-
+Log.writeLog = (log)=>{
+    const date = new Date();
+    const file = `${date.getFullYear()}-${date.getMonth() + 1}`;
+    const filepath = path.join(__dirname,`./${file}.log`);
+    if(fs.existsSync(filepath)){
+        fs.appendFileSync(filepath,log+"\r\n");
+    }else{
+        fs.writeFileSync(filepath,log)+"\r\n";
+    }
 }
 
 Log.error = (desc,err)=>{
