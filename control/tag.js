d3af4e7ad1ebfcc8af851f65f0ca3a0c7b67cfa4
crazy.zhou <fenlary.zhou@gmail.com>
2018-08-06 12:03:44 +0800

    根据tag查询相关blog

--- control/tag.js
+++ control/tag.js
@@ -1,5 +1,5 @@
 "use strict";
-const {TagModel} = require("./../model");
+const {TagModel,ArticleModel} = require("./../model");
 const Log = require("./../logs/log");
 exports.list = async(ctx,next)=>{
 
@@ -18,5 +18,20 @@ exports.list = async(ctx,next)=>{
 }
 
 exports.articles = async(ctx,next)=>{
-    
+    const tag = ctx.parms.tag||"";
+
+    if(tag){
+        try{
+            let articles = await ArticleModel.find({tags:tag}).then();
+
+            result.data = articles;
+            result.code = 0;
+            result.msg = "get articles by tag successful.";
+        }catch(err){
+            Log.error("get articles' list by tag",err.message);
+        }
+    }else{
+        result.msg = "params tag empty.";
+    }
+    ctx.body = result;
 }
